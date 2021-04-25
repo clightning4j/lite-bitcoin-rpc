2ce59cf251890d77948c7c40132a35e77092a059
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-04-25 22:09:46 +0200

    Formatting code
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lib/src/main/java/io/github/clightning4j/litebtc/LiteBitcoinRPC.java
+++ lib/src/main/java/io/github/clightning4j/litebtc/LiteBitcoinRPC.java
@@ -63,9 +63,9 @@ public class LiteBitcoinRPC {
       T result = HttpFactory.getInstance().makeRequest(parameters);
       String resultStr = converter.serialization(result);
       return (T) converter.deserialization(resultStr, clazz);
-    } catch(BitcoinCoreException bitcoinCoreException){
+    } catch (BitcoinCoreException bitcoinCoreException) {
       throw bitcoinCoreException;
-    }catch (UtilsExceptions utilsExceptions) {
+    } catch (UtilsExceptions utilsExceptions) {
       throw new LiteBitcoinRPCException(utilsExceptions.getCause());
     }
   }
